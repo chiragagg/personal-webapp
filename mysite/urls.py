ab13cd2b36b43e8afb16bc4ec33d8c0e1100fe90
Chirag Aggarwal <chiraga911@gmail.com>
2018-06-03 13:29:18 +0530

    second commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.conf.urls import url,include
 from django.contrib import admin
 
-urlpatterns = [
+urlpatterns =
+[
     url(r'^admin/', admin.site.urls),
     url(r'^webapp/',include('webapp.urls')),
     url(r'^$',include('personal.urls')),
