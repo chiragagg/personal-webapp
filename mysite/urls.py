7b5b054138e7af368eb49dc296fd901561b11b0b
Chirag Aggarwal <chiraga911@gmail.com>
2018-06-04 23:33:40 +0530

    djanoj based personal web_application

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,8 +17,7 @@ Including another URLconf
 from django.conf.urls import url,include
 from django.contrib import admin
 
-urlpatterns =
-[
+urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^webapp/',include('webapp.urls')),
     url(r'^$',include('personal.urls')),
