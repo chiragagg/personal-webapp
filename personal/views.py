f66000750bd5a954d75d786133af7b561547b788
Chirag Aggarwal <chiraga911@gmail.com>
2018-06-07 21:37:31 +0530

    update Contact

--- personal/views.py
+++ personal/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'personal/home.html') 
+
+def contact(request):
+    return render(request,'personal/basic.html',{'content':['if you would to contact me ,please email me!!!','chiraga911@gmail.com']}) 
